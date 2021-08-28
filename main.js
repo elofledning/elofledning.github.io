d2381dc41601d8664503249e22309da21dad8384
Kim Dyrholm Kristiansen <kimdyrholm@gmail.com>
2021-08-28 22:02:26 +0200

    Added css animation lib

---
+++ main.js
@@ -0,0 +1,7 @@
+let load = () => {
+   /* let div = document.createElement("div");
+    div.id = 'div1';
+    div.appendChild(document.createTextNode('hey div'));
+
+    document.body.appendChild(div);*/
+}
