3b29c480fa808b119a861ccd9f8fc4639d1799be
Kim Dyrholm Kristiansen <kimdyrholm@gmail.com>
2021-08-29 17:17:08 +0200

    Absolute divs

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 let load = () => {
-   /* let div = document.createElement("div");
-    div.id = 'div1';
-    div.appendChild(document.createTextNode('hey div'));
+    let div = document.createElement("div");
+    div.id = 'div'+1;
+    div.classList.add('blockdiv');
+    div.classList.add('animate__animated');
+    div.classList.add('animate__backOutUp');
+    div.style.left = '30%';
+    div.style.animationDelay = '0.1s';
 
-    document.body.appendChild(div);*/
+    
+    document.body.appendChild(div);
 }
